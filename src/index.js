db8f9c7e5b99a41e2e76e0b7db32c3c62caace8e
Arif Naga Pratama <arifpratama398@gmail.com>
2022-08-21 13:23:55 +0700

    step 3 done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import './app.scss';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import * as serviceWorker from './serviceWorker';
 import 'core-js/modules/es7.array.includes';
 import 'core-js/modules/es6.array.fill';
@@ -10,10 +11,24 @@ import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache: cache,
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
